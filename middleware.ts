d5a8f11c19f91e9243871bbe53aa8305ee5d4d75
Aiyaret Sandhu <arshsandhuprofessional@gmail.com>
2025-03-07 18:35:46 +0530

    feat : deployment issue resolution state 1

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export default withAuth(
   function middleware(req) {
-    
+    // Log the token/session information
     console.log('Token in middleware:', req.nextauth.token);
     return NextResponse.next();
   },
@@ -20,6 +20,5 @@ export default withAuth(
 export const config = {
   matcher: [
     '/',
-    '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
   ]
 };
